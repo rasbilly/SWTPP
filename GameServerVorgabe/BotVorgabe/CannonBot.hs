8192dff70e1c312bea7ccfed4c5ba34f27a8aa70
furny <furny@DESKTOP-PH67D01.localdomain>
2018-01-11 19:53:29 +0100

    BotVorgabe added

---
+++ GameServerVorgabe/BotVorgabe/CannonBot.hs
@@ -0,0 +1,12 @@
+--- module (NICHT AENDERN!)
+module CannonBot where
+--- imports (NICHT AENDERN!)
+import Data.Char
+import Util
+
+--- external signatures (NICHT AENDERN!)
+getMove :: String -> String
+listMoves :: String -> String
+
+--- YOUR IMPLEMENTATION STARTS HERE ---
+
